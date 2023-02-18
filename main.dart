2714b5836c702917fb06730ca6a4ded9d3b71c46
Pintu kewat <pintookewat88@gmail.com>
2023-02-18 03:43:36 -0800

    [changes]

--- main.dart
+++ main.dart
@@ -3,48 +3,48 @@
 // runtimeType its check the data type
 
 void main(){
-	print("Hello Dart");
+print("Hello Dart");
 
 
 // Varibale name  in dart
-	 var address =  "hello";
- 	print(address);
+var address =  "hello";
+print(address);
 
- // Built in datatype
- int cart_item = 10;
- double price = 10.50;
- String name = "Pintoo";
- bool is_complete = true;
- var quantity = 100;
+// Built in datatype
+int cart_item = 10;
+double price = 10.50;
+String name = "Pintoo";
+bool is_complete = true;
+var quantity = 100;
 
 // Arthmetic operators
-	var a = 10;
-	var b = 20;
-	var c = 50;
-	var d = 3;
+var a = 10;
+var b = 20;
+var c = 50;
+var d = 3;
 
-	print(a + b);  
-	print(b - c);
-	print(c * a);
-	print(d / b); // 3.33333
-	print(d ~/ b);  // 3
-	print(a % d); // 1 reminder  
+print(a + b);  
+print(b - c);
+print(c * a);
+print(d / b); // 3.33333
+print(d ~/ b);  // 3
+print(a % d); // 1 reminder  
 
-	// prefix and postfix inmcrement and decrement
-	 
-	print(" ++ or --");
+// prefix and postfix inmcrement and decrement
 
-	// Equality and relational operators
- 
-	print("== , !=, <, > , >=, <= ");
- 
- // Logical operators
- print(" && , ||");
+print(" ++ or --");
 
-	// Type test Operator 
+// Equality and relational operators
 
-	var name0 = "pintoo";
- print(name0 is String);
+print("== , !=, <, > , >=, <= ");
+
+// Logical operators
+print(" && , ||");
+
+// Type test Operator 
+
+var name0 = "pintoo";
+print(name0 is String);
 
 // Assigement operators
 var test = 10;
@@ -54,10 +54,10 @@ print(test);
 
 // conditonal operators
 
-	var is_login =  true;
-	// condition ? ex1 : ex2
+var is_login =  true;
+// condition ? ex1 : ex2
 
-	// num ??  "test" if there is null value then its give test output
+// num ??  "test" if there is null value then its give test output
 
 
 // Expression inside string
@@ -68,14 +68,14 @@ print('${name1.toUpperCase()}');
 print('${name1.toLowerCase()}');
 
 // Row string
-	var sting  = r"Hello welcome to dart \n code";
-	print(sting);
+var sting  = r"Hello welcome to dart \n code";
+print(sting);
 
-	// string method
-	print(sting.isEmpty);	
-	print(sting.isNotEmpty);	
-	print(sting.contains("hello"));
-	print(sting.padLeft(10));
-	print(sting.padRight(10));
+// string method
+print(sting.isEmpty);	
+print(sting.isNotEmpty);	
+print(sting.contains("hello"));
+print(sting.padLeft(10));
+print(sting.padRight(10));
 
 }
